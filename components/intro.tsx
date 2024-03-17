8122480af44c0e9b2ec3dfe08b57545a84c2396e
charbel-ayrouth <ayrouthcharbel@gmail.com>
2024-03-17 21:45:06 +0200

    Skills section

--- components/intro.tsx
+++ components/intro.tsx
@@ -30,8 +30,8 @@ export default function Intro() {
               alt="Ricardo portrait"
               width="192"
               height="192"
-              quality="95"
-              priority={true}
+              // quality="95"
+              // priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
