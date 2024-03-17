6137fc0e026589697abe1370c197239150e0f756
charbel-ayrouth <ayrouthcharbel@gmail.com>
2024-03-17 20:57:20 +0200

    Header with Active Section and scrolling

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,10 +6,16 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section
+      id="home"
+      ref={ref}
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-96">
       <div className="flex justify-center items-center">
         <div className="relative">
           <motion.div
---
+++ context/active-section-context.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useState, createContext, useContext } from "react";
+import type { SectionName } from "@/lib/types";
+
+// we are not importing a server component here
+// and we are using Children, so we can still have children component as server component
+
+type ActiveSectionContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>; //hover over seters function
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+};
+
+// default value is used if we try to access outside the provider component
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
+
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}>
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within a ActiveSectionContextProvider"
+    );
+  }
+
+  return context;
+}
